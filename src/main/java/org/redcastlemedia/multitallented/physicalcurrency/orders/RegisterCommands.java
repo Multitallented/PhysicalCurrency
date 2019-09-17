e46e08a1c5c0e16da5631954bfacb471e9f865c3
Dan Miller <dmiller@accurate.com>
2019-09-17 14:05:22 -0700

    register commands properly

--- src/main/java/org/redcastlemedia/multitallented/physicalcurrency/orders/RegisterCommands.java
+++ src/main/java/org/redcastlemedia/multitallented/physicalcurrency/orders/RegisterCommands.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 
 import org.redcastlemedia.multitallented.physicalcurrency.commands.Generic;
 import org.redcastlemedia.multitallented.physicalcurrency.commands.PCurrCommand;
+import org.redcastlemedia.multitallented.physicalcurrency.commands.Pay;
+import org.redcastlemedia.multitallented.physicalcurrency.commands.Reload;
 
 public final class RegisterCommands {
     private RegisterCommands() {
@@ -11,11 +13,12 @@ public final class RegisterCommands {
     }
     public static HashMap<String, PCurrCommand> execute() {
         HashMap<String, PCurrCommand> commands = new HashMap<>();
-        commands.put("pay", null); // TODO make paycommand
+        commands.put("pay", new Pay());
         Generic generic = new Generic();
         commands.put("pcurr", generic);
         commands.put("money", generic);
         commands.put("bal", generic);
+        commands.put("reload", new Reload());
         return commands;
     }
 }
