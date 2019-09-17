5240d59fb5785306cca07ab9b1d329a594287e3c
Dan Miller <dmiller@accurate.com>
2019-09-17 12:02:27 -0700

    added the ability to withdraw from an online player's physical inventory

---
+++ src/main/java/org/redcastlemedia/multitallented/physicalcurrency/orders/TransferPhysicalToAccount.java
@@ -0,0 +1,60 @@
+package org.redcastlemedia.multitallented.physicalcurrency.orders;
+
+import java.util.ArrayList;
+
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.redcastlemedia.multitallented.physicalcurrency.ConfigManager;
+import org.redcastlemedia.multitallented.physicalcurrency.accounts.Account;
+import org.redcastlemedia.multitallented.physicalcurrency.accounts.AccountManager;
+import org.redcastlemedia.multitallented.physicalcurrency.util.ItemUtil;
+
+public final class TransferPhysicalToAccount {
+    private TransferPhysicalToAccount() {
+
+    }
+    public static double execute(Player player, double amount) {
+        ItemStack singleItem = ConfigManager.getInstance().getSingleItem();
+        ItemStack nineItem = ConfigManager.getInstance().getNineItem();
+        ItemStack eightOneItem = ConfigManager.getInstance().getEightyOneItem();
+        Account account = AccountManager.getInstance().getAccount(player.getUniqueId());
+
+        double amountTaken = 0;
+        ArrayList<ItemStack> removeThese = new ArrayList<>();
+        for (ItemStack itemStack : player.getInventory()) {
+            if (amountTaken >= amount) {
+                break;
+            }
+            if (itemStack == null || (itemStack.getType() != singleItem.getType() &&
+                    itemStack.getType() != nineItem.getType() &&
+                    itemStack.getType() != eightOneItem.getType())) {
+                continue;
+            }
+            if (ItemUtil.isEquivalentItem(itemStack, singleItem)) {
+                amountTaken += addStackToAmount(itemStack, amount, amountTaken, 1, removeThese);
+            } else if (ItemUtil.isEquivalentItem(itemStack, nineItem)) {
+                amountTaken += addStackToAmount(itemStack, amount, amountTaken, 9, removeThese);
+            } else if (ItemUtil.isEquivalentItem(itemStack, eightOneItem)) {
+                amountTaken += addStackToAmount(itemStack, amount, amountTaken, 81, removeThese);
+            }
+        }
+        player.getInventory().removeItem((ItemStack[]) removeThese.toArray());
+        return amountTaken;
+    }
+    private static int addStackToAmount(ItemStack itemStack,
+                                 double amount,
+                                 double amountTaken,
+                                 int denomination,
+                                 ArrayList<ItemStack> removeThese) {
+        int stackAmount = itemStack.getAmount() * denomination;
+        if (amount - amountTaken > stackAmount) {
+            removeThese.add(itemStack);
+            return stackAmount;
+        } else {
+            int amountAdded = (int) Math.ceil(amount - amountTaken);
+            int newStackAmount = stackAmount - (int) Math.ceil((double) amountAdded / denomination);
+            itemStack.setAmount(newStackAmount);
+            return amountAdded;
+        }
+    }
+}
--- src/main/java/org/redcastlemedia/multitallented/physicalcurrency/orders/WithdrawPlayer.java
+++ src/main/java/org/redcastlemedia/multitallented/physicalcurrency/orders/WithdrawPlayer.java
@@ -2,8 +2,10 @@ package org.redcastlemedia.multitallented.physicalcurrency.orders;
 
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
 import org.redcastlemedia.multitallented.physicalcurrency.accounts.Account;
 import org.redcastlemedia.multitallented.physicalcurrency.accounts.AccountManager;
+import org.redcastlemedia.multitallented.physicalcurrency.util.ItemUtil;
 
 import net.milkbowl.vault.economy.EconomyResponse;
 
@@ -17,10 +19,19 @@ public final class WithdrawPlayer {
     }
     public static EconomyResponse execute(OfflinePlayer offlinePlayer, double amount) {
         Account account = AccountManager.getInstance().getAccount(offlinePlayer.getUniqueId());
+        if (offlinePlayer.isOnline()) {
+            Player player = (Player) offlinePlayer;
+            double amountTaken = TransferPhysicalToAccount.execute(player, amount);
+            TransferAccountToPhysical.execute(player);
+            double inventoryAmount = ItemUtil.countCurrencyInInventory(player.getInventory());
+            if (amount <= amountTaken) {
+                return new EconomyResponse(amount, account.getAmount() + inventoryAmount,
+                        EconomyResponse.ResponseType.SUCCESS, "");
+            }
+        }
         double newAmount = account.getAmount() - amount;
         newAmount = newAmount < 0 ? 0 : newAmount;
         account.setAmount(newAmount);
-        // TODO withdraw physical currency
         return new EconomyResponse(amount, newAmount,
                 EconomyResponse.ResponseType.SUCCESS, "");
     }
